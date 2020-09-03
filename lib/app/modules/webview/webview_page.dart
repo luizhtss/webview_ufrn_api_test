27fe28e10f10e2959ddcc5e0919a817710d1c98e
luizhtss <lhtargino@gmail.com>
2020-09-03 10:19:26 -0300

    Update webview_page.dart

--- lib/app/modules/webview/webview_page.dart
+++ lib/app/modules/webview/webview_page.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:webview_ufrn_api/app/modules/httreq.dart';
 import 'package:webview_ufrn_api/credenciais_token.dart';
 import 'webview_controller.dart';
 
@@ -18,17 +19,32 @@ class WebviewPage extends StatefulWidget {
 class _WebviewPageState extends ModularState<WebviewPage, WebviewController> {
   InAppWebViewController webView;
 
-
   @override
   Widget build(BuildContext context) {
-    Credenciais.carregarCredenciais();
-    String url = Credenciais.URL_AUTH + "authz-server/oauth/authorize?client_id=${Credenciais.getClientID()}&response_type=code";
+    String url = Credenciais.URL_AUTH +
+        "authz-server/oauth/authorize?client_id=${Credenciais.getClientID()}&response_type=code&redirect_uri=https://imd.ufrn.br";
     return Scaffold(
-      appBar: AppBar(title: Text("Autenticação obrigatória"), backgroundColor: Colors.green),
+      appBar: AppBar(
+          title: Text("Autenticação obrigatória"),
+          backgroundColor: Colors.green),
       body: InAppWebView(
         initialUrl: url,
+        initialOptions: InAppWebViewGroupOptions(
+            crossPlatform: InAppWebViewOptions(
+          cacheEnabled: false,
+          clearCache: true,
+        )),
         onLoadStop: (InAppWebViewController controller, String url) async {
-          print(url);
+          if (url.startsWith("https://imd.ufrn.br/")) {
+            RegExp regex = new RegExp("code=(.*)");
+            String codigo = regex.firstMatch(url)?.group(1);
+            print("codigo é: $codigo");
+          }
+          if (url.startsWith(
+              "https://autenticacao.info.ufrn.br/sso-server/login?service")) {
+            var html = await controller.evaluateJavascript(source: "window.document.getElementById(\"username\").value");
+            print("HTML: $html");
+          }
         },
       ),
     );
