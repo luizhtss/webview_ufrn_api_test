b65e080c93f31feeab4b63433dbba2bc275e7cab
luizhtss <lhtargino@gmail.com>
2020-09-02 22:10:21 -0300

    Update credenciais_token.dart

--- lib/credenciais_token.dart
+++ lib/credenciais_token.dart
@@ -2,26 +2,27 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart';
 
-class Credenciais{
+class Credenciais {
   static String _clientId;
   static String _clientSecret;
   static String _xApiKey;
 
   static getClientID() => _clientId;
+
   static getClientSecret() => _clientSecret;
+
   static getXApiKey() => _xApiKey;
 
-  static carregarCredenciais(){
-    _carregarCredJSON().then((value){
+  static final URL_AUTH = "https://autenticacao.info.ufrn.br/";
+
+  static Future carregarCredenciais() async {
+    return await rootBundle
+        .loadString("assets/credenciais.json", cache: false)
+        .then((value) {
       var jsonDecoded = json.decode(value);
       _clientId = jsonDecoded['client-id'];
       _clientSecret = jsonDecoded['client-secret'];
       _xApiKey = jsonDecoded['x-api-key'];
     });
   }
-
-  static Future<String> _carregarCredJSON() async {
-    return await rootBundle.loadString("assets/credenciais.json", cache: false);
-  }
-
 }
