205f4ffe642a3d69bf3e19b8797771dd1c443abb
luizhtss <lhtargino@gmail.com>
2020-09-03 02:34:48 -0300

    Update code to use inappwebview plugin

--- lib/app/modules/webview/webview_page.dart
+++ lib/app/modules/webview/webview_page.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:webview_ufrn_api/credenciais_token.dart';
 import 'webview_controller.dart';
 
@@ -16,56 +16,21 @@ class WebviewPage extends StatefulWidget {
 }
 
 class _WebviewPageState extends ModularState<WebviewPage, WebviewController> {
+  InAppWebViewController webView;
 
-  final flutterWebviewPlugin = new FlutterWebviewPlugin();
-
-  StreamSubscription _onDestroy;
-  StreamSubscription<String> _onUrlChanged;
-  StreamSubscription<WebViewStateChanged> _onStateChanged;
-
-  @override
-  void dispose() {
-    _onDestroy.cancel();
-    _onUrlChanged.cancel();
-    _onStateChanged.cancel();
-    flutterWebviewPlugin.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    flutterWebviewPlugin.close();
-
-    _onDestroy = flutterWebviewPlugin.onDestroy.listen((_) {
-      print("killed stream :p");
-    });
-    _onStateChanged =
-        flutterWebviewPlugin.onStateChanged.listen((WebViewStateChanged state) {
-      print("onStateChanged: ${state.type} ${state.url}");
-    });
-
-    _onUrlChanged = flutterWebviewPlugin.onUrlChanged.listen((String url) {
-      if (mounted) {
-        setState(() {
-          print("URL changed: $url");
-        });
-      }
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     Credenciais.carregarCredenciais();
-    //String loginUrl = Credenciais.URL_AUTH + "authz-server/oauth/token?client_id=${Credenciais.getClientID()}&client_secret=${Credenciais.getClientSecret()}&grant_type=client_credentials";
-    String loginUrl = Credenciais.URL_AUTH + "authz-server/oauth/authorize?client_id=${Credenciais.getClientID()}&response_type=code&redirect_uri=http://enderecoapp.com.br/pagina";
-
-    return WebviewScaffold(
-        url: loginUrl,
-        appBar: new AppBar(
-          backgroundColor: Colors.green,
-          title: new Text("Autenticação Obrigatória"),
-        ));
+    String url = Credenciais.URL_AUTH + "authz-server/oauth/authorize?client_id=${Credenciais.getClientID()}&response_type=code";
+    return Scaffold(
+      appBar: AppBar(title: Text("Autenticação obrigatória"), backgroundColor: Colors.green),
+      body: InAppWebView(
+        initialUrl: url,
+        onLoadStop: (InAppWebViewController controller, String url) async {
+          print(url);
+        },
+      ),
+    );
   }
 }
